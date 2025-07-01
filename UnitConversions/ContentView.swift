6176e7060ce4f6a96ec69fc781e1ac0e2bd36693
Sandro Gakharia <sandrogakharia@gmail.com>
2025-07-01 23:41:14 +0400

    Initial commit

--- UnitConversions/ContentView.swift
+++ UnitConversions/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var inputNumber = 1.0
+    @State private var inputUnit = "seconds"
+    @State private var outputUnit = "seconds"
+    @FocusState private var isFocused: Bool
+    let timeUnits = ["seconds", "minutes", "hours", "days"]
+    
+    func convertToSeconds(input: Double, unit: String) -> Double {
+        if unit == "seconds" {
+            return input
+        } else if unit == "minutes" {
+            return input * 60.0
+        } else if unit == "hours" {
+            return input * 60.0 * 60.0
+        } else {
+            return input * 60.0 * 60.0 * 24.0
         }
-        .padding()
+    }
+    
+    func convertToOthers(input: Double, outputUnit: String) -> Double {
+        let seconds = convertToSeconds(input: input, unit: inputUnit)
+        if outputUnit == "seconds" {
+            return seconds
+        } else if outputUnit == "minutes" {
+            return seconds / 60.0
+        } else if outputUnit == "hours" {
+            return seconds / 60.0 / 60.0
+        } else {
+            return seconds / 60.0 / 60.0 / 24.0
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section ("How many units?") {
+                    TextField(inputUnit, value: $inputNumber, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($isFocused)
+                    Picker("From", selection: $inputUnit) {
+                        ForEach(timeUnits, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }.pickerStyle(.navigationLink)
+                    Picker("To", selection: $outputUnit) {
+                        ForEach(timeUnits, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                
+                Section ("Result") {
+                    Text(convertToOthers(input: inputNumber, outputUnit: outputUnit).formatted())
+                }
+            }
+            .navigationTitle("Time Converter")
+            .toolbar {
+                if isFocused {
+                    Button("Done") {
+                        isFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
